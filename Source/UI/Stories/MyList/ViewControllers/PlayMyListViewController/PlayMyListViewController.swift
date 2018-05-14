0896063108c6f1b63905d1d6225d9892241fef53
Liubchenko <liubchenko.v@nixsolutions.com>
2018-05-14 20:10:00 +0300

    Scout | Added custom nabber appearance. Added default tableview realization in list view controller.

--- Source/UI/Stories/MyList/ViewControllers/PlayMyListViewController/PlayMyListViewController.swift
+++ Source/UI/Stories/MyList/ViewControllers/PlayMyListViewController/PlayMyListViewController.swift
@@ -10,4 +10,153 @@ import UIKit
 
 class PlayMyListViewController: UIViewController {
     
+    @IBOutlet fileprivate weak var mainTitleLabel: UILabel!
+    @IBOutlet fileprivate weak var tableView: UITableView!
+    @IBOutlet fileprivate var headerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet fileprivate var titleTopConstraint: NSLayoutConstraint!
+    @IBOutlet fileprivate var gradientButton: GradientButton!
+    
+    fileprivate let maxHeaderHeight: CGFloat = 64
+    fileprivate let minHeaderHeight: CGFloat = 24
+    fileprivate var previousScrollOffset: CGFloat = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configureUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.headerHeightConstraint.constant = self.maxHeaderHeight
+        updateHeader()
+    }
+    
+    // MARK: Private
+    fileprivate func configureUI() {
+        
+        gradientButton.direction = .horizontally(centered: 0.1)
+    }
+}
+
+extension PlayMyListViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "Cell \(indexPath.row)"
+        return cell
+    }
+}
+
+extension PlayMyListViewController: UITableViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let scrollDiff = scrollView.contentOffset.y - self.previousScrollOffset
+        
+        let absoluteTop: CGFloat = 0
+        let absoluteBottom: CGFloat = scrollView.contentSize.height - scrollView.frame.size.height;
+        
+        let isScrollingDown = scrollDiff > 0 && scrollView.contentOffset.y > absoluteTop
+        let isScrollingUp = scrollDiff < 0 && scrollView.contentOffset.y < absoluteBottom
+        
+        if canAnimateHeader(scrollView) {
+            
+            // Calculate new header height
+            var newHeight = self.headerHeightConstraint.constant
+            
+            if isScrollingDown {
+                newHeight = max(self.minHeaderHeight, self.headerHeightConstraint.constant - abs(scrollDiff))
+            }
+            else if isScrollingUp {
+                newHeight = min(self.maxHeaderHeight, self.headerHeightConstraint.constant + abs(scrollDiff))
+            }
+            
+            // Header needs to animate
+            if newHeight != self.headerHeightConstraint.constant {
+                self.headerHeightConstraint.constant = newHeight
+                self.updateHeader()
+                self.setScrollPosition(self.previousScrollOffset)
+            }
+            
+            self.previousScrollOffset = scrollView.contentOffset.y
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        self.scrollViewDidStopScrolling()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            self.scrollViewDidStopScrolling()
+        }
+    }
+    
+    func scrollViewDidStopScrolling() {
+        
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let midPoint = self.minHeaderHeight + (range / 2)
+        
+        if self.headerHeightConstraint.constant > midPoint {
+            self.expandHeader()
+        }
+        else {
+            self.collapseHeader()
+        }
+    }
+    
+    func canAnimateHeader(_ scrollView: UIScrollView) -> Bool {
+        // Calculate the size of the scrollView when header is collapsed
+        let scrollViewMaxHeight = scrollView.frame.height + self.headerHeightConstraint.constant - minHeaderHeight
+        
+        // Make sure that when header is collapsed, there is still room to scroll
+        return scrollView.contentSize.height > scrollViewMaxHeight
+    }
+    
+    func collapseHeader() {
+        
+        self.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.2,
+                        animations: {
+                            self.headerHeightConstraint.constant = self.minHeaderHeight
+                            self.updateHeader()
+                            self.view.layoutIfNeeded()
+                        })
+    }
+    
+    func expandHeader() {
+        
+        self.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.2,
+                        animations: {
+                            self.headerHeightConstraint.constant = self.maxHeaderHeight
+                            self.updateHeader()
+                            self.view.layoutIfNeeded()
+                        })
+    }
+    
+    func setScrollPosition(_ position: CGFloat) {
+        self.tableView.contentOffset = CGPoint(x: self.tableView.contentOffset.x, y: position)
+    }
+    
+    func updateHeader() {
+       
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let openAmount = self.headerHeightConstraint.constant - self.minHeaderHeight
+        let percentage = openAmount / range
+        
+        self.mainTitleLabel.alpha = percentage
+    }
 }
