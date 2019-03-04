96f7cff60945c897efcebd28d7ea3924df19c2a0
Egor Miroshnychenko <egorm@exaltcg.com>
2019-03-04 16:27:58 +0200

    Add subscriptions tab and flow coordinator.

---
+++ Source/FlowCoordinators/SubscriptionsFlowCoordinator.swift
@@ -0,0 +1,56 @@
+//
+//  SubscriptionsFlowCoordinator.swift
+//  Scout
+//
+//
+
+import UIKit
+
+class SubscriptionsFlowCoordinator: BaseFlowCoordinator {
+
+    typealias ShowClosure = (_ content: UIViewController, _ animated: Bool) -> Void
+
+    private let assembly: Assembly
+
+    private lazy var navigationController: UINavigationController = {
+        let navigation = UINavigationController()
+        navigation.setNavigationBarHidden(true, animated: false)
+
+        let navigationBarController = createController()
+
+        navigation.setViewControllers([navigationBarController], animated: false)
+
+        return navigation
+    }()
+
+    private let show: ShowClosure
+
+    init(
+        rootNavigation: RootNavigationProtocol,
+        assembly: Assembly,
+        show: @escaping ShowClosure
+        ) {
+
+        self.assembly = assembly
+        self.show = show
+
+        super.init(rootNavigation: rootNavigation)
+    }
+
+    func showContent(animated: Bool) {
+        show(navigationController, animated)
+    }
+
+    private func createController() -> UIViewController {
+        let navigationBarController = NavigationBarContainerController()
+        _ = navigationBarController.view
+        let navigationBar = DefaultNavigationBar.loadFromNib()
+        navigationBarController.setNavigationBarContent(navigationBar)
+        return navigationBarController
+    }
+}
+
+extension SubscriptionsFlowCoordinator {
+
+    class Assembly { }
+}
