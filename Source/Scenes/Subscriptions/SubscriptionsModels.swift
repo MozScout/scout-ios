29bf1954f3bc5b16129827c1a506603a5b891774
Egor Miroshnychenko <egorm@exaltcg.com>
2019-03-22 19:49:11 +0200

    Added collection views layout

--- Source/Scenes/Subscriptions/SubscriptionsModels.swift
+++ Source/Scenes/Subscriptions/SubscriptionsModels.swift
@@ -18,7 +18,26 @@ enum Subscriptions {
 // MARK: - Models
 
 extension Subscriptions.Model {
-    
+
+    struct SceneModel {
+        var sections: [Section]
+    }
+
+    struct Section {
+        let title: String
+        let items: [Item]
+    }
+
+    struct Item {
+        let iconUrl: URL
+        let title: String
+        let date: Date
+    }
+
+    struct SectionViewModel {
+        let titleModel: SubscriptionsHeaderSupplementaryView.ViewModel
+        let viewModels: [SubscriptionsGroupCell.ViewModel]
+    }
 }
 
 // MARK: - Events
@@ -30,5 +49,26 @@ extension Subscriptions.Event {
         struct Request {}
         struct Response {}
         struct ViewModel {}
+    }
+
+    enum RefreshDidStart {
+        struct Request { }
+    }
+
+    enum RefreshDidFinish {
+        struct Response { }
+        struct ViewModel { }
+    }
+
+    enum SectionsDidUpdate {
+
+        struct Response {
+            let sections: [Model.Section]
+        }
+
+        enum ViewModel {
+            case full([Model.SectionViewModel])
+            case empty
+        }
     }
 }
